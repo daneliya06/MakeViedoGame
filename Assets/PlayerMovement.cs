3046219d24ee9f680d9d1d3d9df5d684786ebe0d
daneliya06 <devopstrat@gmail.com>
2021-11-23 09:22:41 +0700

    Add physics

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,13 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour{
 
     public Rigidbody rb;
-    
-    void Start(){
-        rb.AddForce(0, 1000, 0);
-    }
-
-    
-    void Update(){
-        
+     
+    void FixedUpdate(){
+        rb.AddForce(0, 0, 1000 * Time.deltaTime);
     }
 }
+ 
